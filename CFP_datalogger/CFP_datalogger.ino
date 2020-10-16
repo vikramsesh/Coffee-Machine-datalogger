dd0c0fcf9fc14515f20c8724de01907f796ffac9
Vikram Seshadri <34171350+vikramsesh@users.noreply.github.com>
2020-10-15 23:21:01 -0400

    Update CFP_datalogger.ino

--- CFP_datalogger/CFP_datalogger.ino
+++ CFP_datalogger/CFP_datalogger.ino
@@ -18,37 +18,97 @@
 #include <SPI.h>
 #include <SD.h>
 
+
+char inByte = " ";
+
 const int chipSelect = 4;
 int sd_flag = 0;
 int sd_led = 8;
 
-String base_filename = "CFP";
-int file_number = 1;
-String full_filename = " ";
+String dataString = "";
 
-void update_file_number()
+/*************************************************************/
+/* SD Class */
+class CSVFile
 {
-  // See if the file already exists, if so, see if it has _###
-  // If file doesn't exist, add _###
-  // If file does exists, make _### 1 more
-  full_filename = base_filename + String(file_number) + String(F(".txt"));
-  while (SD.exists(full_filename)) {
-    file_number += 1;
-    full_filename = base_filename + String(file_number) + String(F(".txt"));
-  }
-}
+  public:
+    char filenamechar[100];
+    File dataFile;
+    //SdFat SD;
+    String base_filename;
+    int file_number;
+    String full_filename;
+
+    CSVFile(String bfilename)
+    {
+      base_filename = bfilename;
+      file_number = 1;
+      full_filename = bfilename + String(file_number) + String(F(".csv"));
+      full_filename.toCharArray(filenamechar, 100);
+    }
+
+    void beginNow()
+    {
+      SD.begin(chipSelect);
+    }
+
+    void openFile()
+    {
+      dataFile = SD.open(filenamechar, FILE_WRITE);
+    }
+
+    void closeFile()
+    {
+      dataFile.close();
+    }
+
+    void writeToCSV(String data)
+    {
+      dataFile.print(data);
+//      dataFile.print(String(F("\r\n")));
+    }
+
+    void writeHeaders()
+    {
+      dataFile.println(String(F("Outlet Temp, Boiler Temp,Warm Plate Temp,Max Temp,Calibrated Offset Temp,Pump PWN, Boiler On/Off, PTC On/Off, Flow Rate, Current Block Volume, Total Block Volume, Clean Count, Recipe Size, Recipe Brew, Recipe Block, Recipe Total Volume, Recipe Time "))); //Headers
+    }
+
+    void _update_file_number()
+    {
+      // See if the file already exists, if so, see if it has _###
+      // If file doesn't exist, add _###
+      // If file does exists, make _### 1 more
+
+      while (SD.exists(filenamechar)) {
+        file_number += 1;
+        full_filename = base_filename + String(file_number) + String(F(".csv"));
+        full_filename.toCharArray(filenamechar, 100);
+      }
+    }
+};
+
+//setup csv file object
+String filename = "CFP";
+CSVFile csv(filename);
+
 void setup() {
+
   // initialize both serial ports:
-  Serial.begin(57600);
   Serial1.begin(57600);
+  Serial.begin(57600);
   pinMode (sd_led, OUTPUT);
+  digitalWrite(sd_led, LOW);
 
   // see if the card is present and can be initialized:
   if (SD.begin(chipSelect)) {
-    Serial.println("Card success");
-    sd_flag = 1;
-    update_file_number();
+    csv.beginNow();
+    csv._update_file_number();
+    csv.openFile();
+    csv.writeHeaders();
+    csv.closeFile();
+    Serial.println("Card Present");
   }
+  //  Serial.println(FreeRam());
 
 }
 
@@ -56,19 +116,23 @@ void loop() {
 
   // read from port 1, send to port 0:
   if (Serial1.available()) {
-    digitalWrite(sd_led, LOW);
-    char inByte = Serial1.read();
-    if (sd_flag == 1) {
-      File dataFile = SD.open(full_filename, FILE_WRITE);
-      if (dataFile) {
+
+    inByte = Serial1.read();
+    dataString += inByte;
+
+//    Serial.print(dataString);
+    if (inByte == '\n') {
+      Serial.print(dataString);
+      if (SD.begin(chipSelect)) {
+        csv.openFile();
+
         digitalWrite(sd_led, HIGH);
-        dataFile.print(inByte);
-        dataFile.close();
+        csv.writeToCSV(dataString);
+        
+        csv.closeFile();
+        dataString = "";
       }
-
     }
-    Serial.write(inByte);
     digitalWrite(sd_led, LOW);
-
   }
 }
